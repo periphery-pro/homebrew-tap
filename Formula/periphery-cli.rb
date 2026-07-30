e3faba51c852f076eb482a5eec82c4b23b66e1bf
Ian Leitch <ian@leitch.io>
2026-07-30 13:09:19 +0200

    1.0.0.beta.2

--- Formula/periphery-cli.rb
+++ Formula/periphery-cli.rb
@@ -1,25 +1,25 @@
 class PeripheryCli < Formula
   desc "Periphery"
   homepage "https://periphery.pro"
-  version "1.0.0.beta.1"
+  version "1.0.0.beta.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.1/periphery-cli_1.0.0.beta.1_macos_arm64.zip"
-      sha256 "132fb5f0376027aed26bc2871fd651913d583abb6c5982a133c00fdb508df2e9"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_macos_arm64.zip"
+      sha256 "48b574a6f5c868e8a3e07e3f5afdab39734c3624b23fc84f2380e58c0a837cfd"
     else
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.1/periphery-cli_1.0.0.beta.1_macos_x86_64.zip"
-      sha256 "8a371e5bd36575462362b67524abd2b975c27aa30d3ad64132c158a00a69853b"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_macos_x86_64.zip"
+      sha256 "b7b48cfc2f678b902d62bb4a3f334e5bb6a85dd1dbb265d7bed3c431eda94897"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.1/periphery-cli_1.0.0.beta.1_linux_arm64.zip"
-      sha256 "2ba967a6cab83aedce93890e519ec0210d9284358b150c74e74175eb8d01cf2a"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_linux_arm64.zip"
+      sha256 "bbd4522813f06ddf59e52a88912f36d3de285195699c8dcd41588496cde4a878"
     else
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.1/periphery-cli_1.0.0.beta.1_linux_x86_64.zip"
-      sha256 "69b27a8dc573dc3207a077a709861214d4bb15b0d6bd5c85fd5181a960971953"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_linux_x86_64.zip"
+      sha256 "5fa566769edc68fd5863d55eb804ac988d325f9642e3f26c1ac87b991fff5d07"
     end
   end
 
