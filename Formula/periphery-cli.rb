cf2660d4f9dd4156cb91e34efbe14160bd2e89b8
Ian Leitch <ian@leitch.io>
2026-08-12 11:14:33 +0200

    1.0.0.beta.3

--- Formula/periphery-cli.rb
+++ Formula/periphery-cli.rb
@@ -1,25 +1,26 @@
 class PeripheryCli < Formula
   desc "Periphery"
   homepage "https://periphery.pro"
-  version "1.0.0.beta.2"
+  version "1.0.0.beta.3"
+  license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_macos_arm64.zip"
-      sha256 "48b574a6f5c868e8a3e07e3f5afdab39734c3624b23fc84f2380e58c0a837cfd"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.3/periphery-cli_1.0.0.beta.3_macos_arm64.zip"
+      sha256 "cb64814d35ea782f4cbd8544cf2f801798126668bfd177729a3ce735281410e8"
     else
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_macos_x86_64.zip"
-      sha256 "b7b48cfc2f678b902d62bb4a3f334e5bb6a85dd1dbb265d7bed3c431eda94897"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.3/periphery-cli_1.0.0.beta.3_macos_x86_64.zip"
+      sha256 "6cd4f0fbcc255c550c46157fb750a233e68795d1b16a6e680ead1b77e9f9478e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_linux_arm64.zip"
-      sha256 "bbd4522813f06ddf59e52a88912f36d3de285195699c8dcd41588496cde4a878"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.3/periphery-cli_1.0.0.beta.3_linux_arm64.zip"
+      sha256 "e59b98279eef984d19acd467624f6fb4f04aab43fa6f2dd85fd1d60aba4241cd"
     else
-      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.2/periphery-cli_1.0.0.beta.2_linux_x86_64.zip"
-      sha256 "5fa566769edc68fd5863d55eb804ac988d325f9642e3f26c1ac87b991fff5d07"
+      url "https://github.com/periphery-pro/cli-releases/releases/download/1.0.0.beta.3/periphery-cli_1.0.0.beta.3_linux_x86_64.zip"
+      sha256 "03cbd36db8c62cd744741de76ea53133c08aa366b07e53ef542f43f111388033"
     end
   end
 
@@ -41,10 +42,14 @@ class PeripheryCli < Formula
       bin.install "periphery"
       bin.install Dir["libIndexStore.*"]
     end
+
+    doc.install "LICENSE.md", "THIRD_PARTY_NOTICES.txt"
   end
 
   test do
     system bin/"periphery", "version"
+    assert_path_exists doc/"LICENSE.md"
+    assert_path_exists doc/"THIRD_PARTY_NOTICES.txt"
 
     if OS.mac?
       assert_includes (bin/"periphery").rpaths, "#{MacOS::CLT::PKG_PATH}/usr/lib"
